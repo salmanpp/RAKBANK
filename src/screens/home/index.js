03a7ccaba750b90fbf9d2387b86591141334c644
salmanpp <salmanhamza77@gmail.com>
2022-01-21 21:38:09 +0400

    update

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Fonts from '../../theme/Fonts';
 import { Details } from './components/Details'
 
